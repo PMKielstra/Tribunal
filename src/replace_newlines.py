4a0a350cfbd29251f1dc32aa3dd8b5757f4e3477
Michael Kielstra <>
2023-02-10 21:14:00 -0800

    Merge replace_newlines into app.py

--- src/replace_newlines.py
+++
@@ -1,7 +0,0 @@
-from html import escape
-
-def replace_newlines(str):
-    return "<br>".join(escape(str).split("\n"))
-
-def replaceNewlines(app):
-    app.jinja_env.globals.update(replace_newlines=replace_newlines)
